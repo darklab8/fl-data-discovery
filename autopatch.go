83c85e1f1f7b3b9874d1e8e5e28df642d2c75fe7
dd84ai <dd84ai@gmail.com>
2024-12-30 01:59:17 +0100

    refactor: making autopatcher easier to debug

--- autopatch.go
+++ autopatch.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/darklab8/fl-data-discovery/autopatcher"
+import (
+	"os"
+
+	"github.com/darklab8/fl-data-discovery/autopatcher"
+)
 
 func main() {
+	os.Chdir("./freelancer_folder")
+	println(os.Getwd())
 	autopatcher.RunAutopatcher()
 }
